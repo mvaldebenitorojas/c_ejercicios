338dee30992f7bf610154cfdd5c30e23b322aa4a
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-06 21:41:10 -0400

    Agrego variable de imc

--- 01_basico/variables.c
+++ 01_basico/variables.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	int edad = 30;
-	float altura = 1.75;
-	float peso = 75.2;
+	int edad = 43;
+	float altura = 1.80;
+	float peso = 122.1;
 	int año = 1982;
+	float imc;
 
 	printf("Edad: %d años\n", edad);
 	printf("Altura: %.2f metros\n", altura);
-	printf("Peso: %.2f kilos\n", peso);
+	printf("Peso: %.1f kilos\n", peso);
 	printf("Año: %d\n", año);
+
+	imc = peso/altura;
+
+	printf("Indice de masa: %.3f\n",imc);
 
 	return 0;
 
