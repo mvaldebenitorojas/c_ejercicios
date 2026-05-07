2f02ce4cf3525db6844e998cf8b3268a3cdf3a12
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-06 21:16:30 -0400

    Agrego el ejercicio 2 de variables

---
+++ 01_basico/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int edad = 30;
+	float altura = 1.75;
+	float peso = 75.2;
+	int año = 1982;
+
+	printf("Edad: %d años\n", edad);
+	printf("Altura: %.2f metros\n", altura);
+	printf("Peso: %.2f kilos\n", peso);
+	printf("Año: %d\n", año);
+
+	return 0;
+
+}
+
