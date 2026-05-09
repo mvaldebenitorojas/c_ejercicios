5ef90a8283dd58ad8417b738b4e1bb31a6ab0110
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-09 15:26:08 -0400

    Se tabula el código

--- 02_control_flujo/break.c
+++ 02_control_flujo/break.c
@@ -7,38 +7,40 @@ int opcion, ok;
 
 
 
-do
-{
+    do
+    {
     printf("----------------");
     printf("\n1.-Saludo\n2.-Salir");
     printf("\nOpción: \n");
    
     ok =  scanf("%d", &opcion);
 
-    if(ok !=1)
-    {
-        while (getchar()!= '\n');
-        printf("Error: Entrada inválida\n");
-        continue;
-    }
+        if(ok !=1)
+         {
+            while (getchar()!= '\n');
+            printf("Error: Entrada inválida\n");
+            continue;
+         }
     
      
-    if(opcion == 2)
-    {
-        printf("\nSaliendo...\n");
-        break; /*Si la condición se cumple se termina do*/
-    }
-    else if (opcion == 1)
-    {
-        printf("\nHola\n");
-    }
-    else
-    {
-        printf("Error: Entrada inválida\n");
-    }
-}
-while (1);
+        if(opcion == 2)
+         {
+            printf("\nSaliendo...\n");
+            break; /*Si la condición se cumple se termina do*/
+         }
+    
+        else if (opcion == 1)
+         {
+            printf("\nHola\n");
+         }
 
+        else
+         {
+            printf("Error: Entrada inválida\n");
+         }
+        
+    }
+        while (1);
 
 return 0;
 }
