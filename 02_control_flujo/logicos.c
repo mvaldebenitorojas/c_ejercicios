1b588e5b11092a1f0b69c6910eec414f6a08a276
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-08 20:49:08 -0400

    subo logicos

---
+++ 02_control_flujo/logicos.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	char genero; /*femenino o masculino*/
+       	
+	int edad; /*Declaramos variable*/
+
+	
+	printf("Ingrese su genero (f/m): ");
+	scanf("%c", &genero);
+
+	printf("Ingrese su edad: ");
+	scanf("%d", &edad);
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+	if (genero == 'f') /*Condición femenino*/
+	{
+
+	if(edad >= 18 && edad <=60) /*comprueba si a edad se encuentra en el rango establecido 18/60 */
+	{
+		printf("Edad dentro de rango laboral\n"); /*Si cumple la condición*/
+	}
+
+	else
+	{
+		printf("Edad fuera del rango laboral\n"); /*Si no cumple la condición*/
+	}
+	
+	}
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+	else if (genero == 'm') /*Condicion masculino*/
+	{
+
+	if(edad >= 18 && edad <=65) /*comprueba si a edad se encuentra en el rango establecido 18/65 */
+	
+	{
+		printf("Edad dentro de rango laboral\n"); /*Si cumple la condición*/
+	}
+
+	else
+	{
+		printf("Edad fuera del rango laboral\n"); /*Si no cumple la condición*/
+
+	}
+	
+	return 0;
+	}
+}
