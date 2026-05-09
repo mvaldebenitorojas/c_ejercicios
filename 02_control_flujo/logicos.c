d222b8250459d82991d9c8911748f829f70e7d29
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-08 21:28:30 -0400

    mejora codigo logicos

--- 02_control_flujo/logicos.c
+++ 02_control_flujo/logicos.c
@@ -16,38 +16,25 @@ int main(void)
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-	if (genero == 'f') /*Condición femenino*/
-	{
-
-	if(edad >= 18 && edad <=60) /*comprueba si a edad se encuentra en el rango establecido 18/60 */
+	if (genero == 'f' && edad >= 18 && edad <= 60) /*Condición femenino*/
+	
 	{
 		printf("Edad dentro del rango laboral\n"); /*Si cumple la condición*/
-	}
-
-	else
-	{
-		printf("Edad fuera del rango laboral\n"); /*Si no cumple la condición*/
-	}
-	
 	}
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-	else if (genero == 'm') /*Condicion masculino*/
-	{
-
-	if(edad >= 18 && edad <=65) /*comprueba si a edad se encuentra en el rango establecido 18/65 */
-	
+	else if (genero == 'm' && edad >=18 && edad <= 65 ) /*Condicion masculino*/
+		
 	{
 		printf("Edad dentro del rango laboral\n"); /*Si cumple la condición*/
 	}
-
+/*-------------------------------------------------------------------------------------------------------*/
 	else
 	{
 		printf("Edad fuera del rango laboral\n"); /*Si no cumple la condición*/
-
 	}
 	
 	return 0;
-	}
+	
 }
