2bfda893161fc9d733fe6eb5b38c68fd9876bacc
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-11 23:21:18 -0400

    ejercicio de tipos de ingresos %d, %s, %f

---
+++ 01_basico/ejercicio_06.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    system ("clear");
+
+    char nombre [20]; //linea 18
+    char ciudad [20]; //Linea 76
+    
+    int edad; //linea 50
+    int valido = 0;
+    int i = 0;
+
+    float altura; //linea 63
+    float temperatura; // Linea 108
+
+/*----------------------------------------------------------*/
+
+    printf("Ingrese Nombre:");
+    scanf("%s", nombre);
+    
+    while(1)
+    {
+        valido = 1;
+        i = 0;
+    
+        while (nombre[i] != '\0')
+        {
+            if (!isalpha(nombre[i]))
+        {
+            valido = 0;
+        
+        }
+    
+        i ++;
+    
+        }
+            if (valido == 1)
+            {
+                break;
+            }
+            
+                printf("Error: sólo letras permitidas\n");
+                
+                printf("Ingrese nombre: ");
+                scanf("%s", nombre);
+    }
+
+/*----------------------------------------------------------*/    
+    
+    printf("Ingrese la edad: ");
+
+    while (scanf("%d", &edad) != 1 || edad <0)
+    {  
+        printf("Error: sólo números permitidos\n");
+        
+        while (getchar() != '\n');
+
+        printf("Ingrese la edad: ");
+    }
+
+/*----------------------------------------------------------*/
+    
+    printf("Ingrese altura: ");
+    
+    while (scanf("%f", &altura) != 1 || altura <= 0)
+    {
+        printf("Error: sólo números permitidos\n");
+
+        while (getchar() != '\n');
+
+        printf("Ingrese altura: ");
+    }
+           
+/*---------------------------------------------------------*/
+
+    printf("Ingrese ciudad:");
+    scanf("%s", ciudad);
+    
+    while(1)
+    {
+        valido = 1;
+        i = 0;
+    
+        while (ciudad[i] != '\0')
+        {
+            if (!isalpha(ciudad[i]))
+        {
+            valido = 0;
+        
+        }
+    
+        i ++;
+    
+        }
+            if (valido == 1)
+            {
+                break;
+            }
+            
+                printf("Error: sólo letras permitidas\n");
+                
+                printf("Ingrese ciudad: ");
+                scanf("%s", ciudad);
+    }
+ 
+/*---------------------------------------------------------------*/
+
+    printf("Ingrese temperatura: ");
+    
+    while (scanf("%f", &temperatura) != 1)
+    {
+        printf("Error: sólo números permitidos\n");
+
+        while (getchar() != '\n');
+
+        printf("Ingrese temperatura: ");
+    }
+    system("clear");       
+/*-----------------------------------------------------------*/
+
+    printf("\n\n");
+    printf("----------0----------0----------\n");
+    printf("Nombre: %s\n", nombre );
+    printf("Edad: %d años\n", edad);
+    printf("Altura: %.2f m\n", altura);
+    printf("Ciudad: %s\n", ciudad);
+    printf("Temperatura: %.2f °C\n", temperatura);
+    printf("----------0----------0----------\n\n");
+
+    return 0;
+}
+
+
