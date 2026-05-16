c678a54abd86eee54ddb88b9a9f1782e1650ae0e
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-16 07:09:26 -0400

    for_ejercicio_04.c modificación para ingreso de número con validación de datos

--- 02_control_flujo/for_ejercicio_04.c
+++ 02_control_flujo/for_ejercicio_04.c
@@ -5,8 +5,16 @@ int numero, i; // Variables
 int main(void)
 {
 
-    printf("Ingrese un N°: "); // Solicita número
-    scanf("%d",&numero); // Se almacena el valor %d en &numero
+   printf("Ingrese un N°: "); // Solicita número
+ 
+   while (scanf("%d",&numero) != 1)   
+   {
+        printf("Error: sólo números permitidos\n");
+
+        while (getchar() != '\n');
+
+        printf("Ingrese un N°: ");
+   }
 
     for(i = 1; i <= 10; i++) // contador
     {
