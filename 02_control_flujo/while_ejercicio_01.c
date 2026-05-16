cfb829368917127cec305ba7292d24f1a29c0220
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-16 18:07:25 -0400

    while-ejercicio_01.c imprimir números del 1 al 10 usando while

---
+++ 02_control_flujo/while_ejercicio_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+/*Declaración de variables enteras*/
+	int i = 1;
+    int sup; 
+
+    sup = 10; /*Limite superior*/
+
+
+	while (i <= sup) /*Mientras se cumpla la condición*/
+	{
+        printf("%d\n", i);
+       
+	    i++;	
+    }
+
+return 0;
+}
