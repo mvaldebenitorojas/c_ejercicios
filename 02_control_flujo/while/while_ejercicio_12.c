ac156e3408ea76ef21c8ef1a1fd3cd023d557782
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-30 19:41:19 -0400

    while_ejercicio_12.c menú interactivo con while switch y validación de opciones

--- 02_control_flujo/while/while_ejercicio_12.c
+++ 02_control_flujo/while/while_ejercicio_12.c
@@ -9,47 +9,56 @@ int main(void)
     printf("Ingrese una opción: ");
     scanf("%d", &opcion);
 
+      
     while(opcion != 0)
     {
-        switch(opcion)
-        {
-            case 1:
+        if(opcion < 1 || opcion > 4) //Validación de la opción 
+            {
+                printf("Opción iválida\n");
+            }
+        else
+            {
+
+                switch(opcion)
+                {
+                case 1:
             
                 printf("Ingrese dos números: \n");
                 scanf("%d %d", &a, &b);
                 printf("La suma es: %d\n", a + b);
                 break;
 
-            case 2:
+                case 2:
 
                 printf("Ingrese dos números: \n");
                 scanf("%d %d", &a, &b);
                 printf("La resta es: %d\n", a - b);
                 break;
 
-            case 3:     
+                case 3:     
 
                 printf("Ingrese dos números: \n");
                 scanf("%d %d", &a, &b);
                 printf("La multiplicación es: %d\n", a * b);
                 break;
 
-            case 4:
+                case 4:
 
                 printf("Ingrese dos números: \n");
                 scanf("%d %d", &a, &b);
                 printf("La división es: %d\n", a / b);
                 break;
 
-            default:
+                default:
+
                 printf("Opción inválida\n");
+                }
 
-        }
+            }
 
-    printf("Ingrese una opción: ");
-    scanf("%d", &opcion);
-
-    }   
+            printf("Ingrese una opción: ");
+            scanf("%d", &opcion);
+    }
 
     printf("Ingresó cero, se termina el programa\n");
 
