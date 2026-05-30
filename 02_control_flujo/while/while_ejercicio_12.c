63cd145a4627e8ce91ceeb0e6d6a74f0315fb244
Miguel Valdebenito <mvaldebenitorojas@outlook.cl>
2026-05-30 17:50:10 -0400

    while_ejercicio_12.c menu interactivo con switch y while

---
+++ 02_control_flujo/while/while_ejercicio_12.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int opcion;
+    int a, b;
+
+    printf("Ingrese una opción: ");
+    scanf("%d", &opcion);
+
+    while(opcion != 0)
+    {
+        switch(opcion)
+        {
+            case 1:
+            
+                printf("Ingrese dos números: \n");
+                scanf("%d %d", &a, &b);
+                printf("La suma es: %d\n", a + b);
+                break;
+
+            case 2:
+
+                printf("Ingrese dos números: \n");
+                scanf("%d %d", &a, &b);
+                printf("La resta es: %d\n", a - b);
+                break;
+
+            case 3:     
+
+                printf("Ingrese dos números: \n");
+                scanf("%d %d", &a, &b);
+                printf("La multiplicación es: %d\n", a * b);
+                break;
+
+            case 4:
+
+                printf("Ingrese dos números: \n");
+                scanf("%d %d", &a, &b);
+                printf("La división es: %d\n", a / b);
+                break;
+
+            default:
+                printf("Opción inválida\n");
+
+        }
+
+    printf("Ingrese una opción: ");
+    scanf("%d", &opcion);
+
+    }   
+
+    printf("Ingresó cero, se termina el programa\n");
+
+return 0;
+}
